d4ad64d998a7d0f373ec1f75049db3304c8a77c3
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-04-20 17:21:16 +0700

    initialized views/users folder

---
+++ src/views/users/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>I am User.vue</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/users/Users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>I am Users.vue</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
