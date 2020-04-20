5fb04564703912e1f0f003c1bc408241acfcd270
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-04-20 18:12:04 +0700

    displayed basic user info in the user page

--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    <p>I am User.vue</p>
+    <p class='marginY10px' v-for='(userValue, userKey) in user' :key='userKey'>
+      {{userKey}} : {{userValue}}
+    </p>
   </div>
 </template>
 
 <script>
-export default {
+import userService from '../../services/userservice'
 
+export default {
+  data() {
+    return {
+      user: {}
+    }
+  },
+  mounted() {
+    console.log(this.$route.params.userId)
+    userService.getUserById(this.$route.params.userId)
+      .then(res => {
+        this.user = {...res};
+      })
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.marginY10px {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
